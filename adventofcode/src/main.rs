0706836e4c8737f1a5e888d064344a4270c3f2fe
Matthew S K Fox <matthewskfox@gmail.com>
2025-12-06 19:13:42 +0100

    refactor(cli): make 'part' argument non-optional with default value

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -9,20 +9,20 @@ struct Args {
     #[arg(short, long, value_parser = clap::value_parser!(u8).range(1..=25))]
     day: u8,
 
-    #[arg(short, long, value_parser = clap::value_parser!(u8).range(1..=2))]
-    part: Option<u8>,
+    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..=2))]
+    part: u8,
 }
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    println!("Running Year {} - Day {}.{}", args.year, args.day, args.part.unwrap_or(1));
+    println!("Running Year {} - Day {}.{}", args.year, args.day, args.part);
 
     let result = match (args.year, args.day, args.part) {
-        (2025, 1, Some(1)) => adventofcode::year2025::day01::solve1()?,
+        (2025, 1, 1) => adventofcode::year2025::day01::solve1()?,
 
         _ => {
-            eprintln!("Error: Solution for Year {} Day {}.{} is not implemented yet.", args.year, args.day, args.part.unwrap_or(1));
+            eprintln!("Error: Solution for Year {} Day {}.{} is not implemented yet.", args.year, args.day, args.part);
             return Ok(());
         }
     };
